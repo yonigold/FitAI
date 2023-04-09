2a8c07a98504c465dde10e0bf5e664a5ee0b2578
yoni goldshtein <yoni7022@gmail.com>
2023-04-09 16:03:26 +0300

    c

--- src/TrainingProgramForm.jsx
+++ src/TrainingProgramForm.jsx
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 import { useEffect } from "react";
 import axios from "axios";
 import { Helmet } from "react-helmet-async";
-import { OpanAI } from "openai";
+import { OpenAI } from "openai";
+import { Audio } from 'react-loader-spinner'
 import "@fortawesome/fontawesome-free/css/all.css";
 
 const API_KEY = import.meta.env.VITE_API_KEY;
+
 
 const TrainingProgramForm = () => {
   const [age, setAge] = useState("");
@@ -19,6 +21,8 @@ const TrainingProgramForm = () => {
   const [trainingProgram, setTrainingProgram] = useState("");
   const [loading, setLoading] = useState(false);
   const [formComplete, setFormComplete] = useState(false);
+  const [errors, setErrors] = useState({ ageError: "", weightError: "" });
+
 
   const checkFormComplete = () => {
     if (
@@ -61,6 +65,14 @@ const TrainingProgramForm = () => {
       return;
     }
 
+    const ageError = validateAge(age);
+    const weightError = validateWeight(weight);
+
+    if (ageError || weightError) {
+      setErrors({ ageError, weightError });
+      return;
+    }
+
     setLoading(true);
     await fetch("https://api.openai.com/v1/chat/completions", {
       method: "POST",
@@ -87,6 +99,7 @@ const TrainingProgramForm = () => {
             trainingd days per week: ${exerciseFrequency}
 
             The training program should include a balanced mix of exercises, rest days, and suggestions for progression. Please provide a detailed workout plan with specific exercises, sets, reps, and any necessary instructions.
+            Please format and style the program in a nice and readable way and make sure to add a line break between each day of the training program.
             .`,
           },
         ],
@@ -105,19 +118,53 @@ const TrainingProgramForm = () => {
       });
   };
 
+  const validateAge = (age) => {
+    if (age < 13 || age > 100) {
+      return "You must be between 13 and 100 years old to use this service";
+    }
+    return "";
+  };
+
+  const validateWeight = (weight) => {
+    if (weight < 40 || weight > 220) {
+      return "You must be between 40 and 220 kg to use this service";
+    }
+    return "";
+  };
+
+
+  // const trainingProgramFormatted = (trainingProgram) => {
+  //   const lines = trainingProgram.split("\n");
+  //   const formattedLines = lines.map((line, index) => {
+  //     if (line.startsWith("-")) {
+  //       return <li key={index}>{line.slice(1).trim()}</li>;
+  //     } else if (line.startsWith("*")) {
+  //       return <h4 className="days" key={index} style={{ marginTop: "3.5rem" }}>{line.slice(1).trim()}</h4>;
+  //     } else {
+  //       return <p key={index}>{line}</p>;
+  //     }
+  //   });
+  //   return formattedLines;
+  // };
+
   const trainingProgramFormatted = (trainingProgram) => {
     const lines = trainingProgram.split("\n");
     const formattedLines = lines.map((line, index) => {
       if (line.startsWith("-")) {
         return <li key={index}>{line.slice(1).trim()}</li>;
       } else if (line.startsWith("*")) {
-        return <h4 key={index}>{line.slice(1).trim()}</h4>;
+        return (
+          <div key={index} style={{ marginTop: "1.5rem" }}>
+            <h4>{line.slice(1).trim()}</h4>
+          </div>
+        );
       } else {
         return <p key={index}>{line}</p>;
       }
     });
     return formattedLines;
   };
+
   //   const formattedLines = lines.map((line) => {
   //     return <p>{line}</p>;
   //   });
@@ -166,7 +213,12 @@ const TrainingProgramForm = () => {
               type="number"
               value={age}
               onChange={(e) => setAge(e.target.value)}
+              min="13"
+              max="100"
             />
+              {errors.ageError && <p className="error">{errors.ageError}</p>}
+
+
           </label>
           <label>
             Weight (kg):
@@ -174,7 +226,10 @@ const TrainingProgramForm = () => {
               type="number"
               value={weight}
               onChange={(e) => setWeight(e.target.value)}
+              min="40"
+              max="220"
             />
+            {errors.weightError && <p className="error">{errors.weightError}</p>}
           </label>
           <label>
             Gender:
@@ -231,7 +286,7 @@ const TrainingProgramForm = () => {
               <option value="lose weight">Lose weight</option>
               <option value="gain muscle">Gain muscle</option>
               <option value="improve performance">Improve performance</option>
-              <option value="other">Stay in sahpe</option>
+              <option value="other">Stay in shape</option>
             </select>
           </label>
           <label>
@@ -248,7 +303,7 @@ const TrainingProgramForm = () => {
           <button
             id="btn"
             type="submit"
-            disabled={!formComplete}
+            disabled={!formComplete || loading }
             className="text-rose-600"
           >
             Generate Training Program
@@ -257,12 +312,22 @@ const TrainingProgramForm = () => {
         <div className="training-program">
           Your Training Program:
           {loading && (
-            <p className="loading">
-              Just a few moments, we're creating the perfect workout plan for
-              you !
-            </p>
+            <div class="text-center loading">
+    <div role="status">
+        <svg aria-hidden="true" class="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+        </svg>
+        <p className="loading-text">Just a few moments, we're creating the perfect workout plan for...</p>
+    </div>
+</div>
+            // <p className="loading">
+            //   Just a few moments, we're creating the perfect workout plan for
+            //   you !
+            // </p>
           )}
           {trainingProgram && trainingProgramFormatted(trainingProgram)}
+
           {/* {trainingProgram && trainingProgramFormatted(trainingProgram)} */}
         </div>
       </div>
