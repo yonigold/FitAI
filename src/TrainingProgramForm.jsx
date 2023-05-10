16ee7dc7bdb4972742aea1e75741ded241588c50
yoni goldshtein <yoni7022@gmail.com>
2023-05-10 17:43:33 +0300

    c

--- src/TrainingProgramForm.jsx
+++ src/TrainingProgramForm.jsx
@@ -449,7 +449,7 @@ const TrainingProgramForm = () => {
           </p>
 
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={handleFreeTrialClick}>
-              Subscribe
+              Try Now
             </button>
 
         </div>
